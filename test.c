69df615a753ea1eb0a73bbac66d305deb7ccf290
Infatuate <3528478513@qq.com>
2025-12-11 15:59:46 +0800

    solve problem from c2

--- test.c
+++ test.c
@@ -3,29 +3,16 @@ int sub(int a,int b)
 {
 	return a-b;
 }
-<<<<<<< HEAD
 int sb2(int s,int b)
 {return s*b;}
-int main()
-{
-	printf("Hello World !\n");
-        
-=======
 int add(int a,int b)
 {
 	return a+b;
 }
 int main()
 {
->>>>>>> 2e2b05a93dc47db3b148a33ef36836a17841a25e
 	return 0;
-
-
-
 }
-
-
-
 int add(int a,int b)
 {
 return a+b;
